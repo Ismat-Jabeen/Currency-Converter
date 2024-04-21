4a7f2adbfe5588359d080c1e4c9cbfc9f0214087
Ismat Jabeen <ismatjabeen4@gmail.com>
2024-04-21 02:00:21 -0700

    fifth project

---
+++ index.ts
@@ -0,0 +1,46 @@
+#! /usr/bin/env node
+import inquirer from "inquirer"
+
+
+const currency: any = {
+    USD: 1, // Base Currency
+    EUR: 0.91,
+    GBP: 0.76,
+    INR: 74.57,
+    PKR: 280,
+    DIN: 40
+};
+
+let user_answer = await inquirer.prompt( 
+    [
+        {
+         name: "from",
+         message: "Enter From Currency",
+         type: "list",
+         choices: ["USD", "EUR", "GBP", "INR", "PKR","DIN"]
+        },
+        {
+         name: "to",
+         message: "Enter To Currency",
+         type: "list",
+         choices: ["USD", "EUR", "GBP", "INR", "PKR","DIN"]  
+        },
+        {
+           name: "amount",
+           message: "Enter Your Amount",
+           type: "number"
+
+        }
+    ]
+)
+
+let fromAmount = currency[user_answer.from]
+let toAmount = currency[user_answer.to]
+let amount = user_answer.amount
+let baseAmount = amount/fromAmount // USD Base currency
+                // 15000 pkr/280 =
+let convertedAmount = baseAmount * toAmount
+console.log(convertedAmount);
+console.log(fromAmount);
+console.log(toAmount);
+console.log(amount);
